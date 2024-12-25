2c9d080e250f13598f4fc8d4eaed04c5e254c608
zahid-rahman <rahmanzahid94@gmail.com>
2024-12-25 23:37:38 +0600

    fix: change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Make sure this is 'dist' or change it to match your setup
+  },
+});
